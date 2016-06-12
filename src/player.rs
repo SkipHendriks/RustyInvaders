b4fca668dcb2630cf6bd8ccd4169f8bc34031493
Jip Stave <jipstavenuiter@gmail.com>
2016-06-12 23:39:06 +0200

    player changes

--- src/player.rs
+++ src/player.rs
@@ -10,21 +10,41 @@ pub struct Player {
     pub position: f64,
     pub rotation: f64,
     pub texture: glium::Texture2d,
+    pub vertexBuffer: glium::VertexBuffer<Vertex>,
 }
 
 impl Player {
     pub fn new(display : &Display) -> Player {
+        // Setup texture
         let image = image::load(Cursor::new(&include_bytes!("./img/ship.png")[..]),
             image::PNG).unwrap().to_rgba();
         let image_dimensions = image.dimensions();
         let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions);
         let new_texture = glium::texture::Texture2d::new(display, image).unwrap();
 
+        #[derive(Copy, Clone)]
+        struct Vertex {
+            position: [f32; 2],
+            tex_coords: [f32; 2],
+        }
+
+        implement_vertex!(Vertex, position, tex_coords);
+
+
+        // Setup vertexBuffer
+        let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
+        let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 2.0] };
+        let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [2.0, 0.0] };
+        let shape = vec![vertex1, vertex2, vertex3];
+        let newVertexBuffer = glium::VertexBuffer::new(display, &shape).unwrap();
+
+    
 
         Player {
             position: 0.0,
             rotation: 0.0,
             texture: new_texture,
+            vertexBuffer: newVertexBuffer,
         }
     }
 }
