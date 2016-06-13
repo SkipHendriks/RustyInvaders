9df09ac710d61b1349cd2befc743d3a809a8745f
Jip Stave <jipstavenuiter@gmail.com>
2016-06-13 20:57:03 +0200

    new main file added

---
+++ src/newMain.rs
@@ -0,0 +1,20 @@
+#[marco_use]
+extern crate glium;
+extern crate stopwatch;
+
+use stopwatch::{Stopwatch};
+mod game;
+
+fn main_new() {
+   let game = Game::new();
+   let stopwatch = Stopwatch::new();
+
+
+   loop {
+       
+   }
+
+
+
+
+}
